404ed329c2828ff6d33ac08de3be9a86a8b86690
plab0n <plabon.biswas@orbitax.com>
2023-08-27 20:46:39 +0600

    server init

--- main.go
+++ main.go
@@ -1,9 +1,47 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"net/http"
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Error upgrading connection:", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Client connected")
+
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("Error reading message:", err)
+			return
+		}
+
+		fmt.Printf("Received message: %s\n", p)
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			fmt.Println("Error writing message:", err)
+			return
+		}
+	}
+}
+
 func main() {
-    fmt.Println("Hello, Pigeon!")
+	http.HandleFunc("/ws", handleWebSocket)
+
+	fmt.Println("WebSocket server started on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
